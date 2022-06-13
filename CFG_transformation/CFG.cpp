a46ec5fae40fa7758371582c08b40fc39ecb70b5
Ascain <101872200+Ascian@users.noreply.github.com>
2022-06-13 23:58:06 +0800

    finish basic feature

---
+++ CFG_transformation/CFG.cpp
@@ -0,0 +1,387 @@
+#include "CFG.h"
+
+void CFG::removeNoUseSymb()
+{
+	set<char> N0;
+	set<char> N1;
+	for (auto trans : P) {
+		int flag = 0;
+		for (char ch : trans.second) {
+			if (!T.contains(ch)) {
+				flag = 1;
+				break;
+			}
+		}
+		if (flag == 0) {
+			N1.insert(trans.first);
+		}
+	}
+
+	while (N0 != N1) {
+		N0 = N1;
+		for (auto trans : P) {
+			int flag = 0;
+			for (char ch : trans.second) {
+				if (!(T.contains(ch) || N1.contains(ch))) {
+					flag = 1;
+					break;
+				}
+			}
+			if (flag == 0) {
+				N1.insert(trans.first);
+			}
+		}
+	}
+	N = N1;
+
+	N0.clear();
+	N1.clear();
+	N0.insert(InitSymb);
+	N1 = N0;
+	auto range = P.equal_range(InitSymb);
+	for (auto iter = range.first; iter != range.second; iter++) {
+		for (char ch : iter->second)
+			N1.insert(ch);
+	}
+	while (N0 != N1) {
+		N0 = N1;
+		for (char symbol : N1) {
+			auto range = P.equal_range(symbol);
+			for (auto iter = range.first; iter != range.second; iter++) {
+				for (char ch : iter->second)
+					N1.insert(ch);
+			}
+		}
+	}
+
+	for (auto iter = N.begin(); iter != N.end(); iter++) {
+		if (!N1.contains(*iter)) {
+			P.erase(*iter);
+			auto temp = iter;
+			iter++;
+			N.erase(temp);
+			iter--;	
+		}
+	}
+
+	for (auto iter = T.begin(); iter != T.end(); iter++) {
+		if (!N1.contains(*iter)) {
+			auto temp = iter;
+			iter++;
+			T.erase(temp);
+			iter--;
+		}
+	}
+
+	for (auto iter = P.begin(); iter != P.end(); iter++) {
+		for (char ch : iter->second) {
+			if (!(T.contains(ch) || N.contains(ch))) {
+				auto temp = iter;
+				iter++;
+				P.erase(temp);
+				iter--;
+				break;
+			}
+		}
+	}
+}
+
+void addNoDeltaTrans(multimap<char, string>& newP, const char symbol, const string& str, const set<char>& N1, int i) {
+	while(i < str.size()){
+		if (N1.contains(str[i])) {
+			string newStr(str.substr(0, i));
+			newStr += str.substr(i + 1);
+			addNoDeltaTrans(newP, symbol, newStr, N1, i);
+		}
+		i++;
+	}
+	if(!str.empty())
+		newP.insert({ symbol, str });
+}
+
+void CFG::removeDeltaTrans()
+{
+	set<char> N1;
+	for (auto trans : P) {
+		if (trans.second.empty()) {
+			N1.insert(trans.first);
+		}
+	}
+
+	multimap<char, string> newP;
+	for (auto trans : P) {
+		if(!trans.second.empty())
+			addNoDeltaTrans(newP, trans.first, trans.second, N1, 0);
+	}
+	
+	if (N1.contains(InitSymb)) {
+		int i;
+		for (i = 'A'; i <= 'Z'; i++) {
+			if (!N.contains(i)) {
+				N.insert(i);
+				string temp;
+				temp += InitSymb;
+				newP.insert({ i, temp });
+				newP.insert({ i , "" });
+				InitSymb = i;
+				break;
+			}	
+		}
+		if (i > 'Z')
+			throw "Terminal symbol's amount reached the maximun";
+	}
+	P = newP;
+}
+
+void CFG::removeSingleTrans()
+{
+	multimap<char, string> newP;
+	for (char symbol : N) {
+		set<char> N0;
+		N0.insert(symbol);
+		set<char> N1 = N0;
+		auto range = P.equal_range(symbol);
+		for (auto iter = range.first; iter != range.second; iter++) {
+			if(iter->second.length() == 1 && N.contains(iter->second[0]))
+				N1.insert(iter->second[0]);
+		}
+
+		while (N0 != N1) {
+			N0 = N1;
+			for (char ch : N1) {
+				range = P.equal_range(ch);
+				for (auto iter = range.first; iter != range.second; iter++) {
+					if (iter->second.length() == 1 && N.contains(iter->second[0]))
+						N1.insert(iter->second[0]);
+				}
+			}
+		}
+
+		for (char ch : N1) {
+			range = P.equal_range(ch);
+			for (auto iter = range.first; iter != range.second; iter++) {
+				if (!(iter->second.length() == 1 && N.contains(iter->second[0]))) {
+					newP.insert({ symbol, iter->second });
+				}			
+			}
+		}
+	}
+	P = newP;
+}
+
+void CFG::transformToCNF()
+{
+	multimap<char, string> newP;
+	map<char, char> nonTerminal;
+	for (char symbol : T) {
+		int i;
+		for (i = 'A'; i <= 'Z'; i++) {
+			if (!N.contains(i)) {
+				N.insert(i);
+				string temp;
+				temp += symbol;
+				newP.insert({ i, temp });
+				nonTerminal.insert({ symbol, i });
+				break;
+			}
+		}
+		if (i > 'Z')
+			throw "Terminal symbol's amount reached the maximun";
+	}
+	for (auto& trans : P) {
+		if (trans.second.length() != 1) {
+			for (int i = 0; i < trans.second.length(); i++) {
+				if (T.contains(trans.second[i])) {
+					trans.second[i] = nonTerminal.find(trans.second[i])->second;
+				}
+			}
+		}	
+	}
+
+	map<string, char> terminal;
+	for (auto trans : P) {
+		string restStr = trans.second;
+		if (trans.second.length() >= 2) {
+			for (int i = 0; i < trans.second.length() - 2; i++) {
+				string temp = restStr.substr(restStr.length() - 2, 2);
+				if (terminal.contains(temp)) {
+					restStr = restStr.substr(0, restStr.length() - 2);
+					restStr += terminal.find(temp)->second;
+				}
+				else {
+					int j;
+					for (j = 'A'; j <= 'Z'; j++) {
+						if (!N.contains(j)) {
+							N.insert(j);
+							newP.insert({ j, temp });
+							terminal.insert({ temp, j });
+							restStr = restStr.substr(0, restStr.length() - 2);
+							restStr += j;
+							break;
+						}
+					}
+					if (j > 'Z')
+						throw "Terminal symbol's amount reached the maximun";
+				}
+				
+			}
+		}	
+		newP.insert({ trans.first, restStr });
+	}
+	P = newP;
+}
+
+void CFG::transformToGNF()
+{
+	set<char> newSymbol;
+	multimap<char, string> newP;
+	for (char symbol : N) {
+		multimap<char, string> temp0;
+		multimap<char, string> temp = P;
+		int flag = 0;
+		while (temp0 != temp) {
+			temp0 = temp;
+			temp.clear();
+			auto range = temp0.equal_range(symbol);
+			for (auto iter = range.first; iter != range.second; iter++) {
+				if (iter->second.length() == 0 || iter->second[0] >= symbol) {
+					if (iter->second[0] == symbol)
+						flag = 1;
+					temp.insert(*iter);
+				}
+					
+				else {
+					auto range2 = newP.equal_range(iter->second[0]);
+					for (auto iter2 = range2.first; iter2 != range2.second; iter2++) {
+						string str = iter2->second;
+						str += iter->second.substr(1);
+						temp.insert({ symbol, str });
+					}
+				}
+			}
+		}
+		
+
+		if (flag == 1) {
+			char i;
+			for (i = 'A'; i <= 'Z'; i++) {
+				if (!(N.contains(i) || newSymbol.contains(i))) {
+					newSymbol.insert(i);
+					for (auto trans : temp) {
+						if (trans.second[0] == symbol) {
+							string str = trans.second.substr(1);
+							newP.insert({ i, str });
+							newP.insert({ i, str + i});
+						}
+						else {
+							newP.insert(trans);
+							newP.insert({ trans.first, trans.second + i });
+						}
+					}
+					break;
+				}
+			}
+			if (i > 'Z')
+				throw "Terminal symbol's amount reached the maximun";
+		}
+		else {
+			newP.merge(temp);
+		}
+	}
+	
+	P = newP;
+	newP.clear();
+	auto iter = N.end();
+	for (iter--; iter != N.begin(); iter--) {
+		auto range = P.equal_range(*iter);
+		for (auto iter2 = range.first; iter2 != range.second; iter2++) {
+			if (T.contains(iter2->second[0])) {
+				newP.insert(*iter2);
+			}
+			else {
+				auto range2 = newP.equal_range(iter2->second[0]);
+				for (auto iter3 = range2.first; iter3 != range2.second; iter3++) {
+					string str = iter3->second;
+					str += iter2->second.substr(1);
+					newP.insert({ iter2->first, str });
+				}
+			}
+		}
+	}
+	auto range = P.equal_range(*iter);
+	for (auto iter2 = range.first; iter2 != range.second; iter2++) {
+		if (T.contains(iter2->second[0])) {
+			newP.insert(*iter2);
+		}
+		else {
+			auto range2 = newP.equal_range(iter2->second[0]);
+			for (auto iter3 = range2.first; iter3 != range2.second; iter3++) {
+				string str = iter3->second;
+				str += iter2->second.substr(1);
+				newP.insert({ iter2->first, str });
+			}
+		}
+	}
+
+	for (char symbol : newSymbol) {
+		auto range = P.equal_range(symbol);
+		for (auto iter2 = range.first; iter2 != range.second; iter2++) {
+			if (T.contains(iter2->second[0])) {
+				newP.insert(*iter2);
+			}
+			else {
+				auto range2 = newP.equal_range(iter2->second[0]);
+				for (auto iter3 = range2.first; iter3 != range2.second; iter3++) {
+					string str = iter3->second;
+					str += iter2->second.substr(1);
+					newP.insert({ iter2->first, str });
+				}
+			}
+		}
+	}
+
+	P = newP;
+	N.merge(newSymbol);
+}
+
+
+
+ostream& operator<<(ostream& Ostr, const CFG& cfg)
+{
+	Ostr << "Non-terminal symbol: {";
+	auto iter = cfg.N.begin();
+	Ostr << *(iter++);
+	for (; iter != cfg.N.end(); iter++) {
+		Ostr << ", " << *iter;
+	}
+	Ostr << "}" << endl;
+
+	Ostr << "Terminal symbol: {";
+	iter = cfg.T.begin();
+	Ostr << *(iter++);
+	for (; iter != cfg.T.end(); iter++) {
+		Ostr << ", " << *iter;
+	}
+	Ostr << "}" << endl;
+
+	Ostr << "Grammar production:" << endl;
+	for (char symbol : cfg.N) {
+		Ostr << "   " << symbol << " -> ";
+		auto range = cfg.P.equal_range(symbol);
+		auto iter = range.first;
+		if(iter->second.empty())	
+			Ostr << "¦Å";
+		else
+			Ostr << iter->second;
+		for (iter++; iter != range.second; iter++) {
+			if(iter->second.empty())
+				Ostr << " | " << "¦Å";
+			else
+				Ostr << " | " << (iter)->second;
+		}
+		Ostr << endl;
+	}
+
+	Ostr << "Initial non-terminal symbol: " << cfg.InitSymb << endl;
+	return Ostr;
+}
--- CFG_transformation/CFG_transformation.cpp
+++ CFG_transformation/CFG_transformation.cpp
@@ -1,20 +1,45 @@
-﻿// CFG_transformation.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿
+#include "CFG.h"
 
-#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    CFG cfg;
+    
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        char symbol;
+        cin >> symbol;
+        cfg.addN(symbol);
+    }
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        char symbol;
+        cin >> symbol;
+        cfg.addT(symbol);
+    }
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        char symbol;
+        char str[20];
+        cin >> symbol;
+        cin.get();
+        cin.getline(str, 20);
+        cfg.addP(symbol, str);
+    }
+
+    char symbol;
+    cin >> symbol;
+    cfg.setInitSymb(symbol);
+
+    cfg.removeDeltaTrans();
+    cfg.removeSingleTrans();
+    cfg.removeNoUseSymb();
+    cfg.transformToCNF();
+    cfg.transformToGNF();
+
+    cout << cfg;
+    return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
